e9592c5dc410261afee2826bab29335f9803ea91
Wojciech Krzysztofik <wkrzysztofik@MacBook-Pro-Wojciech.local>
2017-07-09 16:42:25 +0200

    Initial map components.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Map from './map';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Map />
+      </div>
     );
   }
 }
